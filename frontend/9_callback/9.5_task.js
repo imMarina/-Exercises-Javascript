cb299e4a6577fc19cd99e66592b214e72890aad9
imMarina <m.shebukhova@yandex.com>
2023-10-28 13:35:19 +0200

    Callback Function 9.1-9.5

--- frontend/9_callback/9.5_task.js
+++ frontend/9_callback/9.5_task.js
@@ -6,10 +6,42 @@
 // - Напишите функцию `filter`,  которая принимает первым параметром массив, вторым - callback-функцию. Функция `filter` должна создавать новый отфильтрованный массив и возвращать его или возвращать пустой массив `[]`. сallback-функцию должна проверять каждый элемент исходного массива на соответствие заданному условию внутри самой сallback-функции. Она должна принимать два параметра: сам элемент и индекс, а также возвращать булевое значение  (`const result = callback(array[index], index)`, где `result` может быть либо `true`, либо `false`). Если `result` равен `true`, то проверяемый элемент попадает новый массив, если `false`, то не попадает в новый массив.
 // - Передайте в созданную функцию `filter` числовой массив, а в качестве callback-функции - функцию, которая будет проверять элемент на чётность. Если элемент чётный, возвращает `true`, иначе `false`. Callback-функцию можно создавать стрелочным способом.
 
+
+function filter(array, callback) {
+    let newArr = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        let elem = array[i];
+        const result = callback(elem, i);
+        
+            if (result === true) {
+                newArr[newArr.length] = elem;
+                // newArr[i] = elem; // [ 2, <1 empty item>, 42, <3 empty items>, 8 ]
+                // newArr.push(elem);
+            }
+        }
+    return newArr;
+   }
+   
+   function myCallbackFunc(elem, index) {
+       if (elem % 2 === 0) {
+           return true;
+       }
+   }
+   
 const arr1 = [2, 3, 42, 15, 99, 5, 8];
 const newArr1 = filter(arr1, myCallbackFunc);
 console.log(newArr1); // [2, 42, 8];
 
 const arr2 = [1, 3, 5];
 const newArr2 = filter(arr2, myCallbackFunc);
+console.log(newArr2); // [];
+
+
+//  Callback-функция стрелочным способом
+
+const newArr1 = filter(arr1, (elem, index) => (elem % 2 === 0) ? true : false);
+console.log(newArr1); // [2, 42, 8];
+
+const newArr2 = filter(arr2, (elem, index) => (elem % 2 === 0) ? true : false);
 console.log(newArr2); // [];
