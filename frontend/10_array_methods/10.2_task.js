99dec26dbc81cedbe489044d9ace13ba25ba700c
imMarina <m.shebukhova@yandex.com>
2023-10-29 13:53:12 +0100

    arrays 10.2

---
+++ frontend/10_array_methods/10.2_task.js
@@ -0,0 +1,12 @@
+// Задача 10.2
+// Напишите функцию, которая сравнивает два массива, если они равны, вернуть true, иначе false.
+
+const arr1 = [1, 2, 3, 4];
+const arr2 = [1, 2, 3, 4];
+console.log(isEqual(arr1, arr2)); // true
+
+
+function isEqual(firstArr, secondArr) {
+    
+    return firstArr.every((element,index) => element === secondArr[index]) && firstArr.length === secondArr.length;
+}
