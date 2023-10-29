a07a60eea9fb08fae9624113019938f9b184c9e5
imMarina <m.shebukhova@yandex.com>
2023-10-29 14:55:13 +0100

    arrays 10.

---
+++ frontend/10_array_methods/10.4_task.js
@@ -0,0 +1,21 @@
+// Задача 10.4
+
+// Напишите функцию, которая убирает повторяющиеся элементы в массиве. 
+// Постараться сохранить исходный порядок элементов в массиве.
+
+const arr = [1, 2, 2, 3, 3, 4, 5, 6, 6];
+const newArr = unique(arr);
+console.log(newArr); // [ 1, 2, 3, 4, 5, 6 ]
+
+
+function unique(array) {
+
+    let newArr = [];
+    array.forEach((item) => {
+        if (!newArr.includes(item)) {
+            newArr.push(item);
+        }
+    });
+    return newArr;
+}
+
