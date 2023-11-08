67f54216dca263ebed9121f5e1d4080bb66f6743
imMarina <m.shebukhova@yandex.com>
2023-11-08 07:11:25 +0100

    method includes

---
+++ frontend/practicum_array_methods/includes.js
@@ -0,0 +1,56 @@
+// method INCLUDES
+
+// Метод includes() проверяет, содержит ли массив указанный элемент или нет.
+// Этот метод определён у массивов и строк.
+// Для массивов: проверяет, есть ли искомый элемент в массиве.
+// Для строк: проверяет, есть ли искомая подстрока в строке.
+// Возвращает true, если искомый элемент нашёлся и false — если нет
+
+
+// Syntax includes() 
+array.includes(valueToFind, fromIndex);
+
+// Параметры includes():
+// searchValue — значение для поиска.
+// fromIndex (необязательно) — позиция в массиве, с которой начинается поиск. По умолчанию это 0.
+// Примечание: Для отрицательных значений поиск начинается с array.length + fromIndex (отсчет идет от обратного). Например, -1 представляет собой последний элемент.
+
+// Return value 
+// true, если searchValue найдено в каком-то месте массива;
+// false, если searchValue не найдено в массиве.
+
+
+// example 1
+let languages = ["JavaScript", "Java", "C"];
+// проверяем содержит ли массив 'Java'
+let check = languages.includes("Java");
+console.log(check); // true
+
+
+// example 2
+let languages = ["JavaScript", "Java", "C", "C++"];
+// проверяем содержит ли массив 'C'
+let check1 = languages.includes("C"); // true
+// проверяем содержит ли массив 'Ruby'
+let check2 = languages.includes("Ruby"); // false
+
+
+
+// example 2: includes() для поиска с учётом регистра
+// Метод includes() чувствителен к регистру. Например:
+
+let languages = ["JavaScript", "Java", "C", "Python"];
+// проверяем содержит ли массив 'Python'
+let check1 = languages.includes("Python"); // true
+// проверяем содержит ли массив 'python'
+let check2 = languages.includes("python"); // false
+
+
+// example 3: includes() с двумя параметрами
+let languages = ["JavaScript", "Java", "C", "Python"];
+
+// второй аргумент указывает позицию для начала поиска
+let check1 = languages.includes("Java", 2); // false
+
+// поиск начинается с третьего последнего элемента
+let check2 = languages.includes("Java", -3); // true
