f393c12817bcddce839cc1ba3c71c023c08c4805
imMarina <m.shebukhova@yandex.com>
2023-11-30 10:20:47 +0100

    Tasks by method forEach

---
+++ frontend/10_array_methods/forEach.2_task.js
@@ -0,0 +1,20 @@
+// Задача forEach.2
+
+// Дан массив. Для каждого элементы вывести в консоль сам элемент и его тип.
+
+const arr = [1, "Alex", 6, "apple", "1", 12];
+
+arr.forEach((elem) => {
+    let type = typeof elem;
+    console.log(`Элемент: ${elem}, тип: ${type}`);
+});
+
+
+//output
+// "Элемент: 1, тип: number"
+// "Элемент: Alex, тип: string"
+// "Элемент: 6, тип: number"
+// "Элемент: apple, тип: string"
+// "Элемент: 1, тип: string"
+// "Элемент: 12, тип: number"
+
