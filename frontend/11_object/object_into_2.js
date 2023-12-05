a30a4504c94efbf2e041e92c7d183d8e379d46b3
imMarina <m.shebukhova@yandex.com>
2023-12-05 13:17:27 +0100

    info updated

--- frontend/11_object/object_into_2.js
+++ frontend/11_object/object_into_2.js
@@ -102,6 +102,16 @@ console.log(newObj); // { firstName: 'Marina', age: 34, address: 'Ленина 2
 newObj.address = undefined;
 console.log(newObj); // { firstName: 'Marina', age: 34, address: undefined }
 
+// create object by function
+function createUser(name, age) {
+    return {
+        name,
+        age,
+    }
+}
+const newUser = createUser('Lily', 25);
+console.log(newUser); // { name: 'Lily', age: 25 }
+
 
 // массив массивов
 let array1 = Object.entries(newObj);
@@ -237,7 +247,7 @@ console.log(newArr3);
 //   ]
 
 
-
+// example 5
 let arrUsers = []; id: 2
 let arrAddresses = []; userID: 2
 
@@ -251,7 +261,7 @@ let newArr4 = arrUsers.map((user) => {
 console.log(newArr4);
 
 
-// Деструктуризация объектов в JavaScript
+// example 6 - Деструктуризация объектов в JavaScript
 let obj = {
 	year:  2025,
 	month: 12,
@@ -273,6 +283,7 @@ console.log(day);   // выведет 31
 // Object.values(obj) – возвращает массив значений.
 // Object.entries(obj) – возвращает массив пар [ключ, значение].
 
+// example 7
 let user = {
     name: "John",
     age: 30
@@ -281,3 +292,140 @@ let user = {
 Object.keys(user) // ["name", "age"]
 Object.values(user) // ["John", 30]
 Object.entries(user) // [ ["name","John"], ["age",30] ]
+
+
+// example 8 - for in 
+let admin = {
+    name: 'Kate',
+    age: 23,
+
+    // Целочисленное значение будет иметь наивысший приоритет и выводиться по порядку от наименьшего к наибольшему 
+    '45': 45,
+    5: '5',
+
+    // Дробное будет последним
+    '15,2': '15,2',
+}
+console.log(admin);
+// output
+{ 
+//     '5': '5', 
+//     '45': 45, 
+//     name: 'Kate', 
+//     age: 23, 
+//     '15,2': '15,2' 
+// }
+
+for (let key in admin) {
+    // show the keys
+    console.log(key); // 5, 45, name, age, 15,2
+    // show key + value
+    console.log(admin[key]); // 5, 45, Kate, 23, 15,2
+}
+
+// Обратите внимание, что все конструкции «for» позволяют нам объявлять переменную внутри цикла, как, например, let key здесь.
+// Кроме того, мы могли бы использовать другое имя переменной. Например, часто используется вариант "for (let prop in obj)".
+
+// example 9 - свойства упорядочены особым образом: свойства с целочисленными ключами сортируются по возрастанию, остальные располагаются в порядке создания. Разберёмся подробнее.
+// В качестве примера рассмотрим объект с телефонными кодами:
+let codes = {
+    "49": "Германия",
+    "41": "Швейцария",
+    "44": "Великобритания",
+    // ..,
+    "1": "США"
+};
+
+for (let code in codes) {
+    alert(code); // 1, 41, 44, 49
+}
+
+// Целочисленные свойства? Это что?
+// Термин «целочисленное свойство» означает строку, которая может быть преобразована в целое число и обратно без изменений.
+// То есть, "49" – это целочисленное имя свойства, потому что если его преобразовать в целое число, а затем обратно в строку, то оно не изменится. А вот свойства "+49" или "1.2" таковыми не являются:
+// Math.trunc - встроенная функция, которая удаляет десятичную часть
+alert( String(Math.trunc(Number("49"))) ); // "49", то же самое ⇒ свойство целочисленное
+alert( String(Math.trunc(Number("+49"))) ); // "49", не то же самое, что "+49" ⇒ свойство не целочисленное
+alert( String(Math.trunc(Number("1.2"))) ); // "1", не то же самое, что "1.2" ⇒ свойство не целочисленное
+
+
+// example 10 - checking for existence of an object property
+// Проверка существования свойства, оператор «in»
+
+const obj = {
+    class: undefined,
+}
+
+console.log(obj.class); // undefined
+console.log(obj.name); // undefined
+
+console.log('class' in obj); // true
+console.log('name' in obj); // false
+
+// Обратите внимание, что слева от оператора in должно быть имя свойства. Обычно это строка в кавычках.
+// Если мы опускаем кавычки, это значит, что мы указываем переменную, в которой находится имя свойства. Например:
+let user = { age: 30 };
+let key = "age";
+alert( key in user ); // true, имя свойства было взято из переменной key
+
+
+// example 11
+// Квадратные скобки также позволяют обратиться к свойству, имя которого может быть результатом выражения. 
+// Например, имя свойства может храниться в переменной:
+
+let user = {
+    name: "John",
+    age: 30
+};
+
+let key = "name";
+console.log( user.key); // undefined
+console.log( user[key]); // John
+
+
+// example 12
+// Мы можем использовать квадратные скобки в литеральной нотации для создания вычисляемого свойства.
+
+let fruit = prompt("Какой фрукт купить?", "apple");
+
+let bag = {
+  [fruit]: 5, // имя свойства будет взято из переменной fruit
+};
+
+alert( bag.apple ); // 5, если fruit="apple"
+// Смысл вычисляемого свойства прост: запись [fruit] означает, что имя свойства необходимо взять из переменной fruit.
+// И если посетитель введёт слово "apple", то в объекте bag теперь будет лежать свойство {apple: 5}.
+
+// Подведём итог: в большинстве случаев, когда имена свойств известны и просты, используется запись через точку. Если же нам нужно что-то более сложное, то мы используем квадратные скобки.
+
+
+// objects
+// Дополнительные операторы:
+
+// Удаление свойства: delete obj.prop.
+// Проверка существования свойства: "key" in obj.
+// Перебор свойств объекта: цикл for for (let key in obj).
+
+
+// Метод constructor / class
+// В JavaScript конструктор — это функция, которая создает объекты, давая им набор заранее определенных свойств и методов.
+// Конструкторы называют с заглавной буквы, чтобы отличать их от обычных функций
+
+
+class Animal {
+    constructor(color, type) {
+        this.color = color;
+        this.type = type;
+    }
+    showInfo() {
+        console.log(`This is ${this.type}`)
+    }
+}
+
+const animal1 = new Animal('white', 'dog');
+const animal2 = new Animal('black', 'cat');
+
+console.log(animal1); // Animal { color: 'white', type: 'dog' }
+console.log(animal2); // Animal { color: 'black', type: 'cat' }
+animal1.showInfo(); // This is dog
+animal2.showInfo(); // This is cat
