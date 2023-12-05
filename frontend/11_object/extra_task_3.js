b5f135b149ac77e8495e17bf2476097e83e2fef2
imMarina <m.shebukhova@yandex.com>
2023-12-05 13:17:45 +0100

    extra task 3 is ready

---
+++ frontend/11_object/extra_task_3.js
@@ -0,0 +1,24 @@
+// Without. Напишите функцию, которая возвращает новый объект без указанных значений.
+/**
+  * Описание задачи: Напишите функцию, которая возвращает новый объект без указанных значений.
+  * Ожидаемый результат: ({ a: 1, b: 2 }, 'b') => { a: 1 }
+  * Сложность задачи: 2 of 5
+  * @param {Object} object - любой объект
+  * @param {?} args - список значений для удаления
+  * @returns {Object} - новый объект без удаленных значений
+*/
+
+const without = (object, ...args) => {
+    
+    for (let key in object) {
+        if (args.find((elem) => elem === key)) {
+            // delete object[key];
+            object[key] = undefined;
+        }
+    }
+  return object
+};
+
+
+const data = { a: 1, b: 2, c: 3 };
+console.log(without(data, 'b', 'c')); // { a: 1, b: undefined, c: undefined }
