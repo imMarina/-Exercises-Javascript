8d18a93929f65e342448072a4c8ad6580c155e02
imMarina <m.shebukhova@yandex.com>
2023-12-06 20:33:50 +0100

    extra task 5 updated

--- frontend/11_object/extra_task_5.js
+++ frontend/11_object/extra_task_5.js
@@ -19,18 +19,59 @@ console.log(isEqual(data, data2)); // true
 console.log(isEqual(data, data3)); // false
 
 
+// draft, my solution, the first one
+// function isEqual(obj1, obj2) {
+    
+//     let arr1 = Object.entries(obj1);
+//     let arr2 = Object.entries(obj2);
+    
+//     let result = arr1.every( (arr, i) => arr.every( (elem, j) => (elem === arr2[i][j]) ) );
+//     // console.log(result);
+    
+//     if (arr1.length === arr2.length) {
+//         return result;
+//     } else {
+//         return false;
+//     }
+// }
+
+// solution 1 - by Object.entries(obj)
 
 function isEqual(obj1, obj2) {
-    
-    let arr1 = Object.entries(obj1);
-    let arr2 = Object.entries(obj2);
-    
-    let result = arr1.every( (arr, i) => arr.every( (elem, j) => (elem === arr2[i][j]) ) );
-    // console.log(result);
-    
-    if (arr1.length === arr2.length) {
-        return result;
-    } else {
-        return false;
-    }
+  let arr1 = Object.entries(obj1);
+  let arr2 = Object.entries(obj2);
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  
+
+  // option 1
+  // return arr1.every((arr, i) => arr[0] === arr2[i][0] && arr[1] === arr2[i][1]);
+
+  // option 2
+  // return arr1.every(([key, value], i) => key === arr2[i][0] && value === arr2[i][1]);
+
+  // option 3
+  return arr1.every(([key1, value1], i) => {
+            let key2 = arr2[i][0];
+            let value2 = arr2[i][1];
+            return key1 === key2 && value1 === value2;
+  });
 }
+
+
+
+// solution 2 - by Object.keys(obj)
+
+const isEqual = (firstObj, secondObj) => {
+
+    const firstObjKeys = Object.keys(firstObj);
+    const secondObjKeys = Object.keys(secondObj);
+
+    if (firstObjKeys.length !== secondObjKeys.length) {
+      return false;
+    }
+
+    return firstObjKeys.every((key) => firstObj[key] === secondObj[key]);
+};
